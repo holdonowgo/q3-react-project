ed8bcec0baa2b4cea3bb291951190b21f39f6451
Randall Spencer <randallspencer@gmail.com>
2017-04-10 13:44:55 -0700

    Refix merge conflicts

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import logo from './logo.svg';
 import banner from './banner.jpg';
 import banner2 from './woman-dumbell-exercise.svg';
 import Routines from './components/Routines';
-<<<<<<< HEAD
 import Dashboard from './components/Dashboard';
-import NavBar from './components/NavBar';
-=======
-import Navbar from './components/NavBar';
->>>>>>> d495d205a9fbf5b49144d006c03757ce4fd87ee4
 import './App.css';
 
 class App extends Component {
@@ -19,7 +14,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
         <div className="App-header">
           <img src={banner2} className="App-logo" alt="logo" />
           <h2>Welcome to Fit-Init</h2>
@@ -27,10 +21,7 @@ class App extends Component {
         {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
-<<<<<<< HEAD
         <Dashboard />
-=======
->>>>>>> d495d205a9fbf5b49144d006c03757ce4fd87ee4
         <Routines />
       </div>
     );
