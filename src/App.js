e95e0ae2be7f32b3af6f2de212a296fb6622c5bd
Matt <mmuhr88@gmail.com>
2017-04-10 11:45:20 -0700

    got a navbar up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import banner from './banner.jpg';
 import banner2 from './woman-dumbell-exercise.svg';
 import Routines from './components/Routines';
-import NavBar from './components/NavBar';
+import Navbar from './components/NavBar';
 import './App.css';
 
 class App extends Component {
@@ -14,6 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <div className="App-header">
           <img src={banner2} className="App-logo" alt="logo" />
           <h2>Welcome to Fit-Init</h2>
@@ -21,7 +22,6 @@ class App extends Component {
         {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
-          <NavBar />
         <Routines />
       </div>
     );
