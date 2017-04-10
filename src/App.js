a2df0d8539f551c89616a2b40428487d6bb84f7c
Randall Spencer <randallspencer@gmail.com>
2017-04-10 11:17:25 -0700

    Get reducer to receive dispatched action

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import logo from './logo.svg';
 import banner from './banner.jpg';
 import banner2 from './woman-dumbell-exercise.svg';
 import Routines from './components/Routines';
+import NavBar from './components/NavBar';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  };
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={banner2} className="App-logo" alt="logo" />
-          <h2>Welcome to Fit-Bitch</h2>
+          <h2>Welcome to Fit-Init</h2>
         </div>
         {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
+          <NavBar />
         <Routines />
       </div>
     );
-  }
+  };
 }
 
 export default App;
